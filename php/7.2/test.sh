7b469e6ece90f7246db06973479c1b7fe488a1fc
Jens A <ja@8works.de>
2019-04-03 15:30:09 +0200

    Fixes $PATH for global composer bin

--- php/7.2/test.sh
+++ php/7.2/test.sh
@@ -6,6 +6,7 @@ php -v | grep 'with Zend OPcache' || exit 1
 php -v | grep 'with Xdebug' || exit 1
 
 composer --version || exit 1
+phpunit --version || exit 1
 node --version || exit 1
 npm --version || exit 1
 lessc --version || exit 1
